05f8cb83a562fa993d8d440847a3f5a59ff37938
xpress-yourself <sahilgogna16@gmail.com>
2022-06-15 15:25:52 -0400

    Created the problem

---
+++ arrays1.py
@@ -0,0 +1,13 @@
+"""
+Author: Sahil Gogna
+Date: 6-15-22
+Description: Remove Duplicates from Sorted Array
+"""
+
+"""
+Description: removes duplicates from an integer array
+Input: Integer Array
+Output: Number of unique elements and array without duplicates
+"""
+def removeDuplicates():
+    
